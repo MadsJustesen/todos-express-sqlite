07deaa68387e76de3ebf0cd109fd39d74c23c48d
Mads Justesen <madsjustesen1990@gmail.com>
2024-03-13 21:56:06 +0100

    added todo model

--- controllers/api/searchController.js
+++ controllers/api/searchController.js
@@ -1,27 +1,13 @@
-var db = require('../../db');
-
-function mapRows(rows) {
-    return rows.map(function(row) {
-      return {
-        id: row.id,
-        title: row.title,
-        completed: row.completed == 1 ? true : false,
-        url: '/' + row.id,
-        created_at: row.created_at,
-        updated_at: row.updated_at,
-        synchronized: row.synchronized
-      }
-    });
-  }
+const { search } = require('../../models/todo')
 
 exports.search = (req, res) => {
-    searchQuery = req.query.q;
-    if (searchQuery != null && searchQuery.trim().length > 0) {
-        db.all("SELECT * FROM todos WHERE title LIKE ? LIMIT 10", ["%".concat(searchQuery, "%")], function(err, rows) {
-        if (err) {return res.json(err.message)}
-        return res.json(mapRows(rows))
-        });
-    } else {
-        return res.json({ error: { message: "Missing search query"}})
-    }
+  searchQuery = req.query.q;
+  if (searchQuery != null && searchQuery.trim().length > 0) {
+    search(searchQuery, function(err, result) {
+      if (err) {return res.json(err.message)}
+      return res.json(result)
+    })
+  } else {
+    return res.json({ error: { message: "Missing search query"}})
+  }
 }
